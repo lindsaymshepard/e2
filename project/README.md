e10c0977ec7173a7a0e4ec2442eae4128b871195
Lindsay Shepard <lindsaymshepard@icloud.com>
2019-09-14 09:59:13 -0400

    Initialized Project 1

---
+++ project/README.md
@@ -0,0 +1,7 @@
+# Project 1
++ By: Lindsay Shepard 
++ Production URL: <http://p1.lindsayshepard.me
+
+##Outside Resources
+None, other than class materials.
+
---
+++ project/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lindsay Shepard</title>
+    <meta charset="utf-8">
+    </head>
+<body>
+    <h1>Lindsay Shepard</h1>
+    <img src="Lindsay/headshot_resized.jpg" alt="Headshot of Lindsay">
+    <h2> About Me</h2>
+    <p>I currently am taking full time courses at Harvard Extension School in the Master's program for Digital Media Design. This is my last semester and along with the four courses, I am also enrolled in the Capstone Tutorial with anticipation to do the capstone next semester. Before doing this program full time, I was working as a full time freelance digital strategist, mainly helping nonprofits fundraise digitally. After finishing this program, I hope to continue to work in marketing and digitial startegy, but instead work on the product side of marketing within tech.</p>
+</body>
+</html>
