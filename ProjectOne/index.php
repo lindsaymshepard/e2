4c45d08d23fe9e7c90ad652242ec0e0cb629e9fa
Lindsay Shepard <lindsaymshepard@icloud.com>
2019-09-14 10:13:02 -0400

    Updated image folder in index.php

--- ProjectOne/index.php
+++ ProjectOne/index.php
@@ -6,7 +6,7 @@
     </head>
 <body>
     <h1>Lindsay Shepard</h1>
-    <img src="Lindsay/headshot_resized.jpg" alt="Headshot of Lindsay">
+    <img src="images/headshot_resized.jpg" alt="Headshot of Lindsay">
     <h2> About Me</h2>
     <p>I currently am taking full time courses at Harvard Extension School in the Master's program for Digital Media Design. This is my last semester and along with the four courses, I am also enrolled in the Capstone Tutorial with anticipation to do the capstone next semester. Before doing this program full time, I was working as a full time freelance digital strategist, mainly helping nonprofits fundraise digitally. After finishing this program, I hope to continue to work in marketing and digitial startegy, but instead work on the product side of marketing within tech.</p>
 </body>
