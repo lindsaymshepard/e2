8707c5662890da76506946f50e9765b693dc936e
Lindsay Shepard <lindsaymshepard@icloud.com>
2019-09-19 15:42:57 -0400

    Started p2;PHP practice files

---
+++ p2/README.md
@@ -0,0 +1,11 @@
+# Project 2
++ By: Lindsay Shepard 
++ Production URL: <http://p2.lindsayshepard.me>
+
+## Game planning
+
+
+## Outside resources
+
+
+## Notes for instructor
---
+++ p2/index.php
@@ -0,0 +1,22 @@
+<?php require 'index-controller.php'; ?>
+<!doctype html>
+<html lang='en'>
+
+<head>
+
+    <title>Game Name</title>
+    <meta charset='utf-8'> 
+
+</head>    
+
+<body>
+
+    <h1>Game Name</h1>
+ 
+    <h2>Mechanics</h2>
+
+    <h2>Results</h2>
+
+</body>
+
+</html>
